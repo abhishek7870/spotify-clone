8d5f5d1c048d81f2fd6e5dd87f1b0f6193a17a4e
abhishek7870 <krabhishek121995@gmail.com>
2020-08-08 01:16:18 +0530

    Updates

---
+++ precache-manifest.f02bff254af51a9ff1c95bbe7fde46b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614562f6ba81674acf614f8d79444b0d",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "a87ab163c43f1b912c2d",
+    "url": "/spotify-clone/static/css/main.c5c41a21.chunk.css"
+  },
+  {
+    "revision": "42b328259365e83f1077",
+    "url": "/spotify-clone/static/js/2.7598c9f2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.7598c9f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87ab163c43f1b912c2d",
+    "url": "/spotify-clone/static/js/main.65c7b8e5.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
